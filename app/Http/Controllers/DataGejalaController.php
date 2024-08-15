58580e42b5029f17e36653e3c6a7655710aa2f52
ilhamz <ilham.wpt021@gmail.com>
2024-08-15 23:26:56 +0800

    First Commit

---
+++ app/Http/Controllers/DataGejalaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DataGejala;
+use App\Http\Requests\StoreDataGejalaRequest;
+use App\Http\Requests\UpdateDataGejalaRequest;
+
+class DataGejalaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDataGejalaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DataGejala $dataGejala)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DataGejala $dataGejala)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDataGejalaRequest $request, DataGejala $dataGejala)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DataGejala $dataGejala)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/DataPenyakitController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DataPenyakit;
+use App\Http\Requests\StoreDataPenyakitRequest;
+use App\Http\Requests\UpdateDataPenyakitRequest;
+
+class DataPenyakitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDataPenyakitRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DataPenyakit $dataPenyakit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DataPenyakit $dataPenyakit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDataPenyakitRequest $request, DataPenyakit $dataPenyakit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DataPenyakit $dataPenyakit)
+    {
+        //
+    }
+}

c67fe7675754b5e1905a03fa30276ecbd4b94552
ilhamz <ilham.wpt021@gmail.com>
2025-01-09 10:17:47 +0800

    Finalize

--- resources/views/components/Jumbotron.blade.php
+++ resources/views/components/Jumbotron.blade.php
@@ -1,7 +1,7 @@
 <section class="bg-white py-16">
     <div class="container mx-auto flex flex-col md:flex-row items-center px-6">
         <div class="flex-1 text-center md:text-left">
-            <h1 class="text-5xl font-bold text-gray-800">Kenali Penyakit Tanaman Cabai Anda</h1>
+            <h1 class="text-5xl font-bold text-gray-800">Forward chaining</h1>
             <p class="text-xl text-gray-600 mt-4">Identifikasi masalah pada tanaman cabai Anda dengan mudah dan dapatkan solusi yang tepat untuk mengatasi penyakit tersebut.</p>
             <a href="/diagnosa" class="mt-6 inline-flex items-center bg-green-500 text-white font-bold py-2 px-4 rounded hover:bg-green-600">
                 <span class="material-symbols-outlined">digital_wellbeing</span>
